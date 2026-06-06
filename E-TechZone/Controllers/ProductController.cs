4f0d4180946e851b47e92fe5efc97ebd9bf9e0fa
Nguyen Du <nguyendu2004.anhuu@gmail.com>
2026-06-06 21:57:04 +0700

    prod details

--- E-TechZone/Controllers/ProductController.cs
+++ E-TechZone/Controllers/ProductController.cs
@@ -1,16 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_TechZone.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_TechZone.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly DataContext _dataContext;
+        public ProductController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Details()
+        public async Task<IActionResult> Details(int Id)
         {
-            return View();
+            if(Id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var productById = _dataContext.Products
+               .Where(p => p.Id == Id).FirstOrDefault();
+            return View(productById);
         }
 
     }
