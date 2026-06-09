eb447f117be96f1d1aa9af4b3a235c34bee10531
Nguyen Du <nguyendu2004.anhuu@gmail.com>
2026-06-09 17:57:15 +0700

    add cart item

--- E-TechZone/Controllers/CartController.cs
+++ E-TechZone/Controllers/CartController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_TechZone.Models;
+using E_TechZone.Models.ViewModels;
+using E_TechZone.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_TechZone.Controllers
 {
     public class CartController : Controller
     {
+        private readonly DataContext _dataContext;
+        public CartController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<CartItemModel> cartsItems = HttpContext.Session.GetJson<List<CartItemModel>>("Cart") ?? new List<CartItemModel>();
+            CartItemViewModel cartVM = new()
+            {
+                CartItems = cartsItems,
+                GrandTotal = cartsItems.Sum(x => x.Quantity * x.Price)
+            };
+            return View(cartVM);
         }
         public IActionResult Checkout()
         {
